2d47066d6eaa0e45399d7106960f66efbfeab0fa
Thina <frog7134@gmail.com>
2023-05-04 15:02:38 +0900

    [Feat] email, gender 입력 기능 추가

--- flask과제/main.py
+++ flask과제/main.py
@@ -2,13 +2,19 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def main():
+    return render_template('main.html')
 
 @app.route('/result', methods=['POST'])
 def result():
     name = request.form['name']
     student_number = request.form['student_number']
     major = request.form['major']
-    return render_template('result.html', name=name, student_number=student_number, major=major)
+    email_id = request.form['email_id']
+    email_addr = request.form['email_addr']
+    gender = request.form['gender']
+    return render_template('result.html', name=name, student_number=student_number, major=major, email_id=email_id, email_addr=email_addr, gender=gender)
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
